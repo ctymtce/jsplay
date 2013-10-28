4ceb9399d09c6a21fb7f34c6b27613efc4b56c4e
ctymtce <ctymtce@126.com>
2013-10-28 15:51:26 +0800

    Update jsplay.js

--- jsplay.js
+++ jsplay.js
@@ -3,3 +3,8 @@ function replaceEnter(str){
     return str.replace(/(\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029))/g, '');
 }
 
+
+//tr实现slideUp用jqery 
+tr.find('td').animate({'paddingTop':0,'paddingBottom':0,height:0}).wrapInner('<div style="display: block;" />') .parent().find('td > div').slideUp(500, function(){
+    //$(this).parent().parent().remove(); 
+    tr.remove();});
