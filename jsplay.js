489796857d5c98626ef536bbe70daddee61ddabc
cty <ctymtce@gmail.com>
2015-07-15 14:41:02 +0800

    Update jsplay.js

--- jsplay.js
+++ jsplay.js
@@ -8,6 +8,11 @@ function replaceEnter(str){
 tr.find('td').animate({'paddingTop':0,'paddingBottom':0,height:0}).wrapInner('<div style="display: block;" />') .parent().find('td > div').slideUp(500, function(){
     //$(this).parent().parent().remove(); 
     tr.remove();});
+//tr实现slideDown用jqery 
+$('#my_table > tbody > tr.my_row').find('td').wrapInner('<div style="display: none;" />').parent().find('td > div').slideDown(700, function(){
+  var $set = $(this);
+  $set.replaceWith($set.contents());
+ });
 
 //js的反调试
 (function(){(function(c){(function d(){try{var u=function(x){if(1!==(""+x/x).length||0===x%20)(function(){}).constructor("debugger")();else debugger;u(++x)};u(0)}catch(e){c.setTimeout(d,50)}})()})(document.body.appendChild(document.createElement("frame")).contentWindow)})();
